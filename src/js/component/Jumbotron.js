3729bdecd580c6ebe0aea03da06f240d92c61663
ColbyWTaylor <colbyt.093@gmail.com>
2019-07-19 12:51:44 +0000

    first

---
+++ src/js/component/Jumbotron.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+export default class Jumbotron extends React.Component {
+	render() {
+		return (
+			<div className="text-center mt-5">
+				<h1>Jumbotron!</h1>
+			</div>
+		);
+	}
+}
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Nav } from "./Nav"; //Nav's file uses "export class Nav extends React.Component", so we need the "{}"
+import Jumbotron from "./Jumbotron"; //Jumbotron's file uses "export default class Jumbotron extends React.Component", so we don't need the "{ }"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -8,18 +10,20 @@ export class Home extends React.Component {
 	render() {
 		return (
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+				<h1>
+					This is the Home Page, and and we are importing other files
+					to organize our content.
+				</h1>
+				<h2>Below we are using the Nav Info file with</h2>
+				<Nav />
+				<h2>Below we are using the Jumbotron file:</h2>
+				<Jumbotron />
+				<h2>
+					Next step is to duplicate a file in the component folder,
+					such as Nav, and update the export inormation (think in
+					terms of what you want that collection of information to be
+					referred to when it is needed)
+				</h2>
 			</div>
 		);
 	}
